8fcd0ecabf5d3dbba1e129c9496ae954f3b14f33
Kaizoku22 <pdeepankar46@gmail.com>
2025-09-19 21:33:57 +0530

    working on displayOnMap

--- src/showOnMap.js
+++ src/showOnMap.js
@@ -1,25 +1,62 @@
-let showonmapbuttons = document.querySelectorAll(".displayOnMapButton");
-
-//console.log(showonmapbuttons);
-
-// loop through each button and attach event listeners
-// loop through each button and attach event listeners
-Array.from(showonmapbuttons).forEach(button => {
-    console.log(button);
-//    button.addEventListener('click', function() {
-//        console.log('showonmap button clicked !!');
-//        displaygeojson('json');
-//    });
-})
-
-
-function displaygeojson(geojson){
-//    console.log('logging displaygeojson')
-//    console.log('logging geojson from post : ',geojson);
-//    const geojsonobject = JSON.parse(geojson);
-//    map.addsource('uploaded-source',{
-//        'type':'geojson',
-//        'data': geojsonobject
-//    });
-    console.log('clicked displaygeoJson button !!!');
+function attachShowOnMapListeners(scope=document){
+    let showonmapbuttons = document.querySelectorAll(".displayOnMapButton");
+//    console.log('LOGGING buttons array: ',showonmapbuttons);
+    Array.from(showonmapbuttons).forEach((button) => {
+        console.log(button);
+        if(!button.dataset.listenerAttached){
+            button.addEventListener('click',function(){
+                displayOnMap(button.dataset.geojson);
+            });
+        }
+        button.dataset.listenerAttached = "true";
+    });
 }
+
+
+    function displayOnMap(geojson){
+        console.log('logging geojson from post : ',geojson);
+        const geojsonobject = JSON.parse(geojson);
+        const cords = geojsonobject.features[0].geometry.coordinates;
+        const lang = cords[0] ;
+        const lat = cords[1];
+        console.log(`lang:${lang} lat:${lat}`)
+        if(map.getSource('post-source')){
+            map.removeSource('post-source');
+        }
+        map.addSource('post-source',{
+            'type':'geojson',
+            'data': geojsonobject
+        });
+        map.addLayer({
+            id:'post-source-layer',
+            type:'circle',
+            source:'post-source',
+            paint:{
+                'circle-radius':8,
+                'circle-color':'#ff9800'
+            }
+        });
+        onClickMapDrawer();
+        map.flyTo({
+            center:[lang,lat],
+            essential:true,
+            zoom:14,
+            offset:[400,200]
+        });
+    }
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    attachShowOnMapListeners();
+});
+
+document.addEventListener('htmx:afterSwap',function(evt){
+    const requestPath = evt.detail.requestConfig.path;
+//    console.log('LOGGING path in afterSwap:',requestPath);
+    if(requestPath === '/trailwall'){
+        console.log('LOGGING HTMX afterSwap detail.xhr',evt.detail.requestConfig.path);
+        attachShowOnMapListeners();
+    }
+
+});
+
